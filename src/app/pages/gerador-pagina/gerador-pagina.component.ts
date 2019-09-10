79d407b1a5070ab006d3911565edfd3b2f9266a1
Maria Eduarda Spiess <maria.spiess@senior.com.br>
2019-09-10 12:05:35 -0300

    gerador de páginas ta quase

--- src/app/pages/gerador-pagina/gerador-pagina.component.ts
+++ src/app/pages/gerador-pagina/gerador-pagina.component.ts
@@ -9,13 +9,36 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class GeradorPaginaComponent implements OnInit {
 
+    exampleText = `O Observatório do Empreendedorismo e Inovação (OBSEI) foi criado em 2017, como um projeto de pesquisa
+                    que tem sido implementado e desenvolvido por professores e pesquisadores da Faculdade SOCIESC de Blumenau.
+                    O OBSEI atualmente está inserido no grupo de pesquisa em Gestão e Tecnologia da Informação e Conhecimento, 
+                    integrado ao conselho nacional de desenvolvimento científico e tecnológico (CNPq/DGP).`
+
     form = new FormGroup({
         headline: new FormControl(),
         texto: new FormControl(),
-        imagem: new FormControl()
+        logo: new FormControl(),
+        descricaoLogo: new FormControl()
     })
 
     constructor() { }
 
     ngOnInit() { }
+
+    uploadFile() {
+        const element = document.querySelector('#input-file') as HTMLElement;
+        element.click();
+    }
+
+    changeFile(inputValue: any) {
+        var file: File = inputValue.files[0];
+        this.form.patchValue({descricaoLogo: file.name});
+        
+        var myReader:FileReader = new FileReader();
+      
+        myReader.onloadend = (e) => {
+            this.form.patchValue({logo: myReader.result});
+        }
+        myReader.readAsDataURL(file);
+      }
 }
