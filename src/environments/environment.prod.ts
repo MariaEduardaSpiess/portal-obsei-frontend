9ebfb194123a5cd3ecd7ae44adfe1bbfc58f071e
dudaspiess <dudaspiess@gmail.com>
2019-10-18 17:01:36 -0300

    adicionando variáveis de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const baseUrl = 'https://portal-obsei-api.herokuapp.com';
+
+const API_HEROKU = {
+    getPesquisadores: baseUrl + '/pesquisadores',
+    postPesquisadores: baseUrl + '/pesquisador',
+    postGeradorPagina: baseUrl + '/pagina-pesquisa'
+}
+
+
 export const environment = {
-  production: true
+  production: true,
+  api: API_HEROKU
 };
