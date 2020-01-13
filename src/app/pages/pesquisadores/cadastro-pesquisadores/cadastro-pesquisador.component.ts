0671e996fccebd2f2ca1a84789fbbfda2043890b
Maria Eduarda Spiess <maria.spiess@senior.com.br>
2020-01-13 11:38:08 -0300

    ajuste no cadastro de pesquisadores

--- src/app/pages/pesquisadores/cadastro-pesquisadores/cadastro-pesquisador.component.ts
+++ src/app/pages/pesquisadores/cadastro-pesquisadores/cadastro-pesquisador.component.ts
@@ -5,7 +5,8 @@ import { PesquisadoresService } from '../pesquisadores.service';
 
 @Component({
     selector: 'cadastro-pesquisador',
-    templateUrl: 'cadastro-pesquisador.component.html'
+    templateUrl: 'cadastro-pesquisador.component.html',
+    styleUrls: ['cadastro-pesquisador.component.scss']
 })
 
 export class CadastroPesquisadorComponent implements OnInit {
@@ -14,9 +15,29 @@ export class CadastroPesquisadorComponent implements OnInit {
     form = new FormGroup({
         nome: new FormControl('', [Validators.required]),
         funcao: new FormControl('', [Validators.required]),
-        lattes: new FormControl('', [Validators.required])
+        lattes: new FormControl('', [Validators.required]),
+        foto: new FormControl('', [Validators.required]),
+        descricaoFoto: new FormControl('', [Validators.required])
     });
+
     ngOnInit() { }
+
+    uploadFile() {
+        const element = document.querySelector('#input-file') as HTMLElement;
+        element.click();
+    }
+
+    changeFile(inputValue: any) {
+        var file: File = inputValue.files[0];
+        this.form.patchValue({ descricaoFoto: file.name });
+
+        var myReader: FileReader = new FileReader();
+
+        myReader.onloadend = (e) => {
+            this.form.patchValue({ foto: myReader.result });
+        }
+        myReader.readAsDataURL(file);
+    }
 
     save(): void {
         this.utils.validateForm(this.form);
